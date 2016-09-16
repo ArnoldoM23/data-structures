36576060426478b87a0c6e4692ec923344db6293
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-09-15 23:27:39 -0700

    improved linked list and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,39 +1,74 @@
-var Queue = function(){
+var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
+  var someInstance = {};
+  _.extend(someInstance, queueMethods);
 
- var someinstance = {};
- someinstance.storage = {};
- someinstance.storageSize = 0;
- someinstance.counter = 0;
- someinstance.removing = 0
- _.extend(someinstance, queueMethods);
-return someinstance;
+  someInstance._storage = {};
+  someInstance._start = 0;
+  someInstance._end = 0;
 
+  return someInstance;
 };
-
 
 var queueMethods = {};
 
-queueMethods.enqueue = function(value){
-    this.storageSize++;
-  return  this.storage[this.counter++] = value;
+queueMethods.enqueue = function(value) {
+  this._storage[this._end++] = value;
+};
 
-  };
+queueMethods.dequeue = function() {
+  if (this._end !== this._start) {
+    var result = this._storage[this._start];
+    delete this._storage[this._start];
+    this._start++
+    return result;
+  }
+};
 
-  queueMethods.dequeue = function(){
-    var toBeRemoved = this.storage[this.removing];
-    delete this.storage[this.removing];
-    this.removing++;
-    this.storageSize --;
-    if (this.storageSize <= 0){
-      this.storageSize = 0;
-    } 
+queueMethods.size = function() {
+  console.log(this._end, 'start= ', this._start)
+  return this._end - this._start;
+};
 
-    return toBeRemoved;
-  };
-  queueMethods.size = function(){
-    return this.storageSize ;
-  };
+
+// var Queue = function(){
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+
+
+//  var someinstance = {};
+//  someinstance.storage = {};
+//  someinstance.storageSize = 0;
+//  someinstance.counter = 0;
+//  someinstance.removing = 0
+//  _.extend(someinstance, queueMethods);
+// return someinstance;
+
+// };
+
+
+// var queueMethods = {};
+
+// queueMethods.enqueue = function(value){
+//     this.storageSize++;
+//   return  this.storage[this.counter++] = value;
+
+//   };
+
+//   queueMethods.dequeue = function(){
+//     var toBeRemoved = this.storage[this.removing];
+//     delete this.storage[this.removing];
+//     this.removing++;
+//     this.storageSize --;
+//     if (this.storageSize <= 0){
+//       this.storageSize = 0;
+//     } 
+
+//     return toBeRemoved;
+//   };
+//   queueMethods.size = function(){
+//     return this.storageSize ;
+//   };
 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,35 +4,44 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    if (list.head === null) {
-      this.head = Node(value);
-      this.tail = this.head;
-    }else {
-      this.tail.next = Node(value);
-      this.tail = this.tail.next
+    if (!list.head) {
+      list.head = Node(value);
+      list.tail = list.head;
+    }else{
+      list.tail.next = Node(value);
+      list.tail = list.tail.next; 
     }
   };
 
-  list.removeHead = function (){
-//if check to see if head is not null; -->  remove the head
-//then point the head to the tail.
-//tail.next will to head.next
-  var current = this.head;
-  var previous = this.head.value
-  if (this.head !== null){
-   this.head = this.tail;
-  }
- return previous;
+  list.removeHead = function(){
+    if (list.head !== null) {
+      var chopHead = list.head.value;
+      list.head = list.head.next;
+      return chopHead;
+    }
+    return null;
   };
 
   list.contains = function(target){
-    if (this.head.value === target){
-      return true;
-    } else if (this.tail.value === target){
-      return true;
-    } else {
-      return false
-    } 
+    if (!list.head) { return false; }
+    // using a while loop
+    // var current = list.head;
+    // while(current){
+    //   if (current.value === target) {
+    //     return true;
+    //   }
+    //   current = current.next;
+    // }
+    // return false;
+
+    // recursively
+    return (function recurse(node) {
+      if (!node) { return false}
+      if (node.value === target) {
+        return true;
+      }
+      return recurse(node.next)
+    })(list.head);
   };
 
   return list;
@@ -47,7 +56,58 @@ var Node = function(value){
   return node;
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
+// my original solution haha
 
- */
+// var LinkedList = function(){
+//   var list = {};
+//   list.head = null;
+//   list.tail = null;
+
+//   list.addToTail = function(value){
+//     if (list.head === null) {
+//       this.head = Node(value);
+//       this.tail = this.head;
+//     }else {
+//       this.tail.next = Node(value);
+//       this.tail = this.tail.next
+//     }
+//   };
+
+//   list.removeHead = function (){
+// //if check to see if head is not null; -->  remove the head
+// //then point the head to the tail.
+// //tail.next will to head.next
+//   var current = this.head;
+//   var previous = this.head.value
+//   if (this.head !== null){
+//    this.head = this.tail;
+//   }
+//  return previous;
+//   };
+
+//   list.contains = function(target){
+//     if (this.head.value === target){
+//       return true;
+//     } else if (this.tail.value === target){
+//       return true;
+//     } else {
+//       return false
+//     } 
+//   };
+
+//   return list;
+// };
+
+// var Node = function(value){
+//   var node = {};
+
+//   node.value = value;
+//   node.next = null;
+
+//   return node;
+// };
+
+// /*
+//  * Complexity: What is the time complexity of the above functions?
+
+//  */
