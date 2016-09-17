f9bd297fd64c453227fc27f6224f7dee1c4ba151
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-09-17 15:35:50 -0700

    improved tre

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,52 +1,94 @@
-
 var Tree = function(value){
   var newTree = {};
-  _.extend(newTree, treeMethods)
+  _.extend(newTree, treeMethods);
   newTree.value = value;
-  newTree.counter = 0;
-  // your code here
-  newTree.children = {};  // fix me
+  newTree.children = [];
+  newTree.parent = null;
   return newTree;
 };
+
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-  this.children[this.counter++] = new Tree(value);
+  var child = Tree(value);
+  child.parent = this;
+  this.children.push(child);
 };
-treeMethods.contains = function(target){
-  var state = false
 
-  function recur (children, target){
-    for(var key in children){
-      if (children[key].value === target) {
-        state= true; 
-      } else if (Object.keys(children[key]).length > 0){
-        recur(children[key].children, target)
-      } else {
-        state = false;
-      }
+treeMethods.traverse = function(target,cb){
+  if (this.children.length < 1) { return false}
+  var flag = false;
+  function searchChildren(node, index){
+    if(node.value === target){
+      node.index = index;
+      return flag = node;
     }
-    return state;
+    if (cb) {node.value = cb(node.value);}
+    for (var i = 0; i < node.children.length; i++) {
+      searchChildren(node.children[i], i)
+    }
   }
-    return recur(this.children, target);
-  // for(var key in this.children){
-  //   if (this.children[key].value === target){
-  //     return true;
-  //   }else if (this.children[key][0]){
-  //     for(var key in this.children[key]){
-  //       if (this.children[key][0].value === target) {
-  //         return true;
-  //       } 
-  //     }
-  //   } else 
-  //   {
-  //     return false;
-  //   }
-  // }
+  searchChildren(this)
+  return flag;
+}
+
+treeMethods.contains = function(target){
+  return this.traverse(target) ? true: false;
 };
 
+treeMethods.removeFromParent = function(value){
+  var foundNode = this.traverse(value);
+  if (foundNode) {
+    foundNode.parent.children.splice(foundNode.index, 1);
+    foundNode.parent = null;
+  }
+}
+
+
+function Node(value){
+  this.value = value;
+  this.children = [];
+}
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+  Complexity: What is the time complexity of the above functions?
+*/
+
+
+// My original solution
+
+// var Tree = function(value){
+//   var newTree = {};
+//   _.extend(newTree, treeMethods)
+//   newTree.value = value;
+//   newTree.counter = 0;
+//   // your code here
+//   newTree.children = {};  // fix me
+//   return newTree;
+// };
+
+// var treeMethods = {};
+
+// treeMethods.addChild = function(value){
+//   this.children[this.counter++] = new Tree(value);
+// };
+// treeMethods.contains = function(target){
+//   var state = false
+//   function recur (children, target){
+//     for(var key in children){
+//       if (children[key].value === target) {
+//         state= true; 
+//       } else if (Object.keys(children[key]).length > 0){
+//         recur(children[key].children, target)
+//       }
+//     }
+//     return state;
+//   }
+//   return recur(this.children, target);
+// };
+
+
+// /*
+//  * Complexity: What is the time complexity of the above functions?
+//  */
